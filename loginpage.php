26867980faad9a5c1f5297d574e6c05da1549228
ARJAYYY22 <fajardiarjay@gmail.com>
2024-11-17 15:36:54 -0800

    login done!

--- loginpage.php
+++ loginpage.php
@@ -22,4 +22,50 @@
     $message = "";
     $alertClass = "alert-info";
 
+    if ($_SERVER["REQUEST_METHOD"] === "POST") {
+        $selectedAccount = $_POST["Accounts"];
+        $enteredUsername = $_POST["username"];
+        $enteredPassword = $_POST["password"];
+
+        $isValid = false;
+        foreach ($users as $user) {
+            if ($user["User Type"] === $selectedAccount && $user["Username"] === $enteredUsername && $user["Password"] === $enteredPassword) {
+                $isValid = true;
+                $message = "Welcome to the system, " . htmlspecialchars($user["Username"]) . "!";
+                break;
+            }
+        }
+
+        if (!$isValid) {
+            $message = "Incorrect username or password.";
+            $alertClass = "alert-danger";
+        }
+    }
+    ?>
+
+    <?php if ($message): ?>
+        <div class="alert <?php echo $alertClass; ?> text-center"><?php echo $message; ?></div>
+    <?php endif; ?>
+
   
+    <div class="card card-container">
+        <img id="profile-img" class="profile-img-card" src="//ssl.gstatic.com/accounts/ui/avatar_2x.png" />
+        <p id="profile-name" class="profile-name-card"></p>
+
+        <form class="form-signin" method="post">
+            <select name="Accounts" class="form-control" id="Accounts" required>
+                <option value="">Select Account Type</option>
+                <option value="Admin">Admin</option>
+                <option value="Content Manager">Content Manager</option>
+                <option value="System User">System User</option>
+            </select>
+            
+            <input type="text" name="username" class="form-control" placeholder="Username" required autofocus>
+            <input type="password" name="password" class="form-control" placeholder="Password" required>
+            
+            <button class="btn btn-lg btn-primary btn-block btn-signin" type="submit">Sign in</button>
+        </form>
+    </div>
+</div>
+</body>
+</html>
